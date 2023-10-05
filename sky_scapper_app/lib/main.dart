b1d01c497a7fb91ea34d9805b590a1042f530dc0
hirenkhasatiya <khasatiyahiren43@gmail.com>
2023-10-05 12:04:07 +0530

    sky scapper

---
+++ sky_scapper_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:sky_scapper_app/views/screens/homePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
